4154ece09ddb21f83dad5d825050730600acb285
masira-syd29 <maseera.sayed29@gmail.com>
2025-01-22 20:23:07 +0530

    commit

---
+++ src/IfElseStatements.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+public class IfElseStatements {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+//        int n = sc.nextInt();
+//        if(n%2==0){
+//            System.out.println("Even");
+//        }
+//        else{
+//            System.out.println("Odd");
+////        }
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int c = sc.nextInt();
+//        if(a>=b && a>=c){
+//            System.out.println(a);
+//        } else if(b>=c && b>=a){
+//            System.out.println(b);
+//        } else {
+//            System.out.println(c);
+//        }
+//        int age = sc.nextInt();
+//        if(age>=18){
+//            System.out.println("Eligible");
+//        } else {
+//            System.out.println("Not Eligible");
+//        }
+//        int marks = sc.nextInt();
+//        if(marks >= 90){
+//            System.out.println("A");
+//        } else if (marks >= 80){
+//            System.out.println("B");
+//        } else if (marks >= 70) {
+//            System.out.println("C");
+//        } else if (marks >= 60) {
+//            System.out.println("D");
+//        }else {
+//            System.out.println("F");
+//        }
+
+//        int year = sc.nextInt();
+//        if((year % 4 == 0 && year%100 != 0) || year %400 == 0){
+//            System.out.println("Leap year");
+//        } else {
+//            System.out.println("Not a Leap Year");
+//        }
+//        int num = sc.nextInt();
+//        if(num == 0){
+//            System.out.println("Zeroes");
+//        } else if (num < 0){
+//            System.out.println("Negative");
+//        } else {
+//            System.out.println("positive");
+//        }
+//        int s1 = sc.nextInt();
+//        int s2 = sc.nextInt();
+//        int s3 = sc.nextInt();
+//        if(s1+s2 > s3 && s2+s3>s1 && s1+s3> s2){
+//            System.out.println("Valid");
+//        }
+//        else {
+//            System.out.println("invalid");
+//        }
+//        int day = sc.nextInt();
+//        if(day == 1){
+//            System.out.println("Monday");
+//        } else if(day == 2){
+//            System.out.println("tuesday");
+//        } else if(day == 3){
+//            System.out.println("Wednesday");
+//        } else if(day == 4){
+//            System.out.println("Thursday");
+//        } else if(day == 5) {
+//            System.out.println("Friday");
+//        } else if(day == 6) {
+//            System.out.println("Saturday");
+//        } else if(day == 7) {
+//            System.out.println("Sunday");
+//        } else {
+//            System.out.println("Invalid");
+//        }
+    }
+}
