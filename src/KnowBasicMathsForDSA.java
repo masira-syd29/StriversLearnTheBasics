0b9ed67dd44ce6290455da0ced7a99ca56b0bf79
imaadsayed <mineimaad@gmail.com>
2025-04-21 21:37:20 +0530

    commit

--- src/KnowBasicMathsForDSA.java
+++ src/KnowBasicMathsForDSA.java
@@ -83,6 +83,21 @@ public class KnowBasicMathsForDSA {
         }
         return true;
     }
+    public static int gcd(int a, int b) {
+        // code here
+        while(a>0 && b>0){
+            if(a>b)
+                a = a%b;
+            else
+                b = b%a;
+
+        }
+        if(a==0)
+            return b;
+        else
+            return a;
+
+    }
 
     public static void main(String[] args) {
 //        int result = countDigit(123888884);
